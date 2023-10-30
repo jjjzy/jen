78283d85a4dc92dbbe88081185ffb137bdbd0476
jjjzy <yujiazhenn@hotmail.com>
2023-10-30 15:08:56 -0500

    docker file change

--- src/main/java/com/example/jenkinss/JenkinssApplication.java
+++ src/main/java/com/example/jenkinss/JenkinssApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @SpringBootApplication
 public class JenkinssApplication {
