db88f76d1cedf5b6eb6d4440f03cf7e378ddeebd
jjjzy <46022826+jjjzy@users.noreply.github.com>
2024-02-08 20:09:52 -0600

    testung (#2)
    * Update JenkinssApplication.java
    
    * Update JenkinssApplication.java

--- src/main/java/com/example/jenkinss/JenkinssApplication.java
+++ src/main/java/com/example/jenkinss/JenkinssApplication.java
@@ -9,13 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class JenkinssApplication {
-    @GetMapping("/test")
-    public String testController(){
-        return "yes";
-    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(JenkinssApplication.class, args);
-    }
 
 }
